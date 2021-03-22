5b2c5aeda65f02a462e74701af182304adc4c150
ukkiy-ray <showy13f207ag@gmail.com>
2021-03-22 17:00:57 +0900

    Article controller編集
    もともと入っていたダミーデータを削除しArticleモデルから全データをコレクションで返しcreated_atの降順で並び替える処理の実装

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,52 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Article;
+
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
     public function index()
     {
-         // ダミーデータ
-         $articles = [
-            (object) [
-                'id' => 1,
-                'title' => 'タイトル1',
-                'body' => '本文1',
-                'created_at' => now(),
-                'user' => (object) [
-                    'id' => 1,
-                    'name' => 'ユーザー名1',
-                ],
-            ],
-            (object) [
-                'id' => 2,
-                'title' => 'タイトル2',
-                'body' => '本文2',
-                'created_at' => now(),
-                'user' => (object) [
-                    'id' => 2,
-                    'name' => 'ユーザー名2',
-                ],
-            ],
-            (object) [
-                'id' => 3,
-                'title' => 'タイトル3',
-                'body' => '本文3',
-                'created_at' => now(),
-                'user' => (object) [
-                    'id' => 3,
-                    'name' => 'ユーザー名3',
-                ],
-            ],
-        ];
+        // もともと入っていたダミーデータを削除
+        
+        $articles = Article::all()->sortByDesc('created_at');
 
         // return view('ビューファイル名', ['ビューに渡す変数名' => 変数の値]);
         return view('articles.index', ['articles' => $articles]);
 
         // 他の書き方(withメソッドを使う方法)
         // return view('articles.index')->with(['articles' => $articles]);
-        
+
         // 他の書き方(compact関数を使う方法)
         // return view('articles.index', compact('articles'));
     }
